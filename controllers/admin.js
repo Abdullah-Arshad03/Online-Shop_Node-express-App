b30527556ec0765577cbee3e425de4541f33dbf8
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2023-10-03 11:31:51 +0500

    relation is made by adding the userId in the product model of the mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,11 +1,11 @@
-const Product = require('../models/product');
-const mongodb = require('mongodb')
+const Product = require("../models/product");
+const mongodb = require("mongodb");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/edit-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    editing: false
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    editing: false,
   });
 };
 
@@ -14,34 +14,44 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = Product({title : title , price : price , description : description , imageUrl : imageUrl})
-  product.save().then((result)=>{
-    console.log('product has been created', result)
-    res.redirect('/admin/products')
-  }).catch((err)=>{
-    console.log('Products isnt created',err) 
-  })
+  const product = Product({
+    title: title,
+    price: price,
+    description: description,
+    imageUrl: imageUrl,
+    userId : req.user
+  });
+  product
+    .save()
+    .then((result) => {
+      console.log("product has been created", result);
+      res.redirect("/admin/products");
+    })
+    .catch((err) => {
+      console.log("Products isnt created", err);
+    });
 };
-
-
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   if (!editMode) {
-    return res.redirect('/');
+    return res.redirect("/");
   }
-  console.log('this is the edit mode',editMode)
+  console.log("this is the edit mode", editMode);
   const prodId = req.params.productId;
-  Product.findById(prodId).then((product)=>{
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: product
+  Product.findById(prodId)
+    .then((product) => {
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: product,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-    
-  }).catch((err)=>{console.log(err)}) 
-  }
+};
 
 exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
@@ -49,36 +59,44 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findById(prodId).then((product)=>{
-    product.title = updatedTitle
-    product.price = updatedPrice
-    product.imageUrl = updatedImageUrl
-    product.description = updatedDesc
-    return product.save()
-  }).then(()=>{
-    res.redirect('/admin/products')
-  }).catch((err)=>{
-    console.log('product isnt updated', err)
-  })
+  Product.findById(prodId)
+    .then((product) => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.imageUrl = updatedImageUrl;
+      product.description = updatedDesc;
+      return product.save();
+    })
+    .then(() => {
+      res.redirect("/admin/products");
+    })
+    .catch((err) => {
+      console.log("product isnt updated", err);
+    });
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.find().then((products)=>{
-    res.render('admin/products', {
-    prods: products,
-    pageTitle: 'Admin Products',
-    path: '/admin/products'
-  })}).catch((err)=>{
-    console.log(err)
-  })
-}
-  
+  Product.find()
+    .then((products) => {
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin Products",
+        path: "/admin/products",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByIdAndRemove(prodId).then(()=>{
-    console.log('product is deleted')
-    res.redirect('/')
-
-  }).catch((err)=>{console.log('product is not deleted', err)})
-  
+  Product.findByIdAndRemove(prodId)
+    .then(() => {
+      console.log("product is deleted");
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log("product is not deleted", err);
+    });
 };
